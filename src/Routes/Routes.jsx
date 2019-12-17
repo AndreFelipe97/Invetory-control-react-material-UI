ca9a7ec46b1cac3bd061c21cad46902e314b6772
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2019-12-17 15:11:01 -0300

    Foi retirado o Browser router

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,17 +1,16 @@
 
 import React from "react";
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 import SuppliersList from '../Components/Suppliers/SuppliersList'
 import SupplierForm from "../Components/Suppliers/SupplierForm";
 
 
 export default props => (
-    <BrowserRouter>
-        <Switch>
-            <Route path='/lista-fornecedores' component={SuppliersList} />
-            <Route path='/cadastro-fornecedores' component={SupplierForm} />
-            <Redirect from='*' to='/lista-fornecedores' />
-        </Switch>
-    </BrowserRouter>
+
+    <Switch>
+        <Route path='/lista-fornecedores' component={SuppliersList} />
+        <Route path='/cadastro-fornecedores' component={SupplierForm} />
+        <Redirect from='*' to='/lista-fornecedores' />
+    </Switch>
 )
