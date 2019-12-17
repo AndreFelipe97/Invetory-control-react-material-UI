a7ab1dffb9502c1cfca8d28a5c022b2295617b2b
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2019-12-17 16:02:00 -0300

    Foi criado um componente personalizado do botão

--- src/Itens/Button.jsx
+++ src/Itens/Button.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 
-export default props => {
-    return (
-        <Button variant={props.variant} color={props.color}>
-            {props.name}
-        </Button>
-    );
-}
+export default props =>
+    <Button variant={props.variant} style={props.style} color={props.color} onClick={props.onClick}>
+        {props.name}
+    </Button>
