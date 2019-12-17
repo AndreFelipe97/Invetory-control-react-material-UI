f77b4401999253bddcbca568e0f6c231c4861f7f
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2019-12-17 16:26:33 -0300

    Foi corrigido as importações do material ui

--- src/Components/Suppliers/SuppliersList.jsx
+++ src/Components/Suppliers/SuppliersList.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Table } from '@material-ui/core';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Grid } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add'
-import { Grid } from "@material-ui/core";
 import { useHistory } from "react-router-dom"
-import Button from '../../Itens/Button'
 
 import api from "../../Services/api";
+import Button from '../../Itens/Button'
+
 
 const useStyles = makeStyles({
     table: {
