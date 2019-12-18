1bbb13e7759ff7c1e045d755f53e07644a91c102
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2019-12-18 16:01:00 -0300

    Foi foi alterado o componente de input

--- src/Itens/Input.jsx
+++ src/Itens/Input.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { Grid, TextField } from '@material-ui/core'
 
 export default props =>
+
     <Grid item {...props.columns}>
         <TextField
             id={props.id}
+            defaultValue ={props.error}
             label={props.label}
             style={props.style}
+            type={props.type}
             required
             value={props.value}
-            onChange={props.value}
+            maxLength={props.maxLength}
+            onChange={props.onChange}
         />
     </Grid>
