66214ee65b1c802c3db42392b292d747b1a8ac78
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2019-12-18 16:00:33 -0300

    Foi criado os campos de cadastro de produtos

--- src/Components/Products/ProductForm.jsx
+++ src/Components/Products/ProductForm.jsx
@@ -1,86 +1,87 @@
-import React, {useState, useEffect} from 'react'
-import {makeStyles, Grid} from "@material-ui/core";
-import {useHistory} from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { makeStyles, Grid } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
-import api from '../../Services/api'
-import Input from '../../Itens/Input'
-import Button from '../../Itens/Button'
+import api from '../../Services/api';
+import Input from '../../Itens/Input';
+import Button from '../../Itens/Button';
+
 
 const useStyles = makeStyles(theme => ({
     root: {
-        '&.MuiTextField-root': {
+        '& .MuiTextField-root': {
             margin: theme.spacing(1),
             width: 200,
-        }
-    }
-}))
+        },
+    },
+}));
 
-export default props => {
+export default function FormPropsTextFields(props) {
     const classes = useStyles();
-    const [values, setValues] = useState(null);
+    const [values, setValues] = useState(null)
 
-    const handleSubmit = (values) => {
+    const handleSubmit = () => {
         console.log(values)
         api.post('/produtos/', values)
             .then(res => console.log(res))
-    }
+    };
 
     const createInstance = () => {
         setValues({
-            name: null,
-            supplier: null,
-            amount: null,
-            buy: null,
-            sale: null,
+            name: '',
+            supplier: '',
+            amount: '',
+            buy: '',
+            sale: '',
         });
     };
 
     const loadInstance = (id) => {
-        api.get(`/fornecedores/${id}`).then(res => {
+        api.get(`/produtos/${id}`).then(res => {
             setValues(res.data)
         }, err => {
             console.log(err)
         });
-    };
+    }
 
     const loadForm = (id) => {
         if (id) {
-            loadInstance(id);
+            loadInstance(id)
         } else {
-            createInstance();
-        }
-        ;
+            createInstance()
+        };
     };
 
     const handleChange = fieldName => event => {
         console.log(fieldName)
         console.log(event.target.value)
-        setValues({ ...values, [fieldName]: event.target.value });
+        setValues({ ...values, [fieldName]: event.target.value })
     }
 
     useEffect((event) => {
         const id = props.match.params['id'] ? props.match.params['id'] : null
-        loadForm(id);
-    }, []);
+        loadForm(id)
+    }, [])
 
-    let history = useHistory();
+    let history = useHistory()
 
     function handleClick() {
-        history.push("/lista-produtos");
+        history.push('/lista-produtos')
     }
 
     return (
         values && <>
             <h3>Cadastro de produtos</h3>
-            <form className={classes.root} noValidate autoComplete="off">
-                <Grid container
-                      direction="row"
-                      justify="flex-start"
-                      alignItems="flex-start"
+            <form className={classes.root} noValidate autoComplete='off'>
+                <Grid
+                    container
+                    direction='row'
+                    justify='flex-start'
+                    alignContent='flex-start'
                 >
                     <Input
                         id='name'
-                        label="Nome"
+                        label='Nome do produto'
                         required
                         columns={{ xs: 12, sm: 12, md: 4, lg: 4, xl: 4 }}
                         style={{ width: '100%', paddingRight: '20px' }}
@@ -88,7 +89,7 @@ export default props => {
                     />
                     <Input
                         id='supplier'
-                        label="Fornecedor"
+                        label='Nome do fornecedor'
                         required
                         columns={{ xs: 12, sm: 12, md: 4, lg: 4, xl: 4 }}
                         style={{ width: '100%', paddingRight: '20px' }}
@@ -96,46 +97,50 @@ export default props => {
                     />
                     <Input
                         id='amount'
-                        label="Quantidade"
+                        label='Quantidade'
                         required
                         columns={{ xs: 12, sm: 12, md: 4, lg: 4, xl: 4 }}
                         style={{ width: '100%', paddingRight: '20px' }}
                         onChange={handleChange('amount')}
                     />
                 </Grid>
-
-                <Grid container>
+                <Grid
+                    container
+                    direction='row'
+                    justify='flex-start'
+                    alignContent='flex-start'
+                >
                     <Input
                         id='buy'
-                        label="Valor de compra"
-                        style={{ width: '100%', paddingRight: '20px' }}
+                        label='Valor de compra'
                         required
-                        columns={{ xs: 12, sm: 12, md: 4, lg: 4, xl: 4 }}
+                        style={{ width: '100%', paddingRight: '20px' }}
+                        columns={{ xs: 12, sm: 12, md: 3, lg: 3, xl: 3 }}
                         onChange={handleChange('buy')}
                     />
                     <Input
                         id='sale'
-                        label="Valor de venda"
-                        style={{ width: '100%', paddingRight: '20px' }}
+                        label='Valor de venda'
                         required
-                        columns={{ xs: 12, sm: 12, md: 4, lg: 4, xl: 4 }}
+                        style={{ width: '100%', paddingRight: '20px' }}
+                        columns={{ xs: 12, sm: 12, md: 3, lg: 3, xl: 3 }}
                         onChange={handleChange('sale')}
                     />
                 </Grid>
-                <Grid container>
+                <Grid container spacing={2}>
                     <Grid item xs={12} sm={12} md={2} lg={2} xl={2}>
                         <Button
                             variant='contained'
-                            style={{ width: '100%', marginTop:'20px' }}
+                            style={{ width: '100%', marginTop: '20px' }}
                             color='primary'
-                            onClick={e => handleSubmit(values)}
+                            onClick={handleSubmit}
                             name='Salvar'
                         />
                     </Grid>
-                    <Grid item xs={12} sm={12} md={2} lg={2} xl={2}>
+                    <Grid xs={12} sm={12} md={2} lg={2} xl={2} >
                         <Button
                             variant='contained'
-                            style={{ width: '100%', marginLeft: '20px', marginTop:'20px' }}
+                            style={{ width: '100%', marginTop: '20px' }}
                             color='secondary'
                             onClick={handleClick}
                             name='Cancelar'
